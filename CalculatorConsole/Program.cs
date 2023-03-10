943b04295c54bc9417fc043ea7ce4ccd669d307f
Mumunz <daaiana.rotar@gmail.com>
2023-03-10 21:38:29 +0200

    More operations

--- CalculatorConsole/Program.cs
+++ CalculatorConsole/Program.cs
@@ -2,6 +2,26 @@
 using System;
 using System.Net.NetworkInformation;
 
+Console.WriteLine(" \n Choose an operation : ");
+
+Console.WriteLine("\n1.Sum");
+Console.WriteLine("2.Subtraction");
+Console.WriteLine("3.Multiplication");
+Console.WriteLine("4.Divison");
+Console.WriteLine("5.Exponentiation");
+Console.WriteLine("6.Even / Odd");
+Console.WriteLine("7.Prime");
+Console.WriteLine("8.Square root");
+Console.WriteLine("9.Mirrored");
+Console.WriteLine("10.Modulus of x");
+Console.WriteLine("11.Palindrome");
+Console.WriteLine("12.SuperPalindrome");
+Console.WriteLine("13.Display palindrome and superpalindrome numbers between 1 and 10000");
+
+
+
+string opertaion = Console.ReadLine();
+
 double number1 = 0;
 
 Console.WriteLine("Enter Number1 :");
@@ -26,6 +46,7 @@ Console.WriteLine(" \n Enter Number2 :");
 
 string theNumberAsAString2 = Console.ReadLine();
 
+
 while (!double.TryParse(theNumberAsAString2, out number2))
 {
     Console.WriteLine(" \n This is not a number. Entere a number : ");
@@ -34,20 +55,10 @@ while (!double.TryParse(theNumberAsAString2, out number2))
 
 Console.WriteLine(" \nThe number is " + number2);
 
-Console.WriteLine(" \n Choose an operation : ");
-
-Console.WriteLine("\n1.Sum");
-Console.WriteLine("2.Subtraction");
-Console.WriteLine("3.Multiplication");
-Console.WriteLine("4.Divison");
-
-
-
-string opertaion = Console.ReadLine();
 
 double operationNumber = 0;
 
-while (!double.TryParse(opertaion, out operationNumber) || (operationNumber != 1 && operationNumber != 2 && operationNumber != 3 && operationNumber != 4))
+while (!double.TryParse(opertaion, out operationNumber) || (operationNumber != 1 && operationNumber != 2 && operationNumber != 3 && operationNumber != 4 && operationNumber != 5 && operationNumber != 6 && operationNumber != 7 && operationNumber != 8 && operationNumber != 9 && operationNumber != 10))
 {
     Console.WriteLine(" \n Choose a valid operation ");
     opertaion = Console.ReadLine();
@@ -57,20 +68,20 @@ while (!double.TryParse(opertaion, out operationNumber) || (operationNumber != 1
 switch (operationNumber)
 {
     case 1:
-        double sumresult = number1 + number2;
+        double sumResult = number1 + number2;
 
-        Console.WriteLine("Sum Result :" + sumresult);
+        Console.WriteLine("Sum Result =" + sumResult);
         break;
     case 2:
-        double subresult = number1 - number2;
+        double subResult = number1 - number2;
 
-        Console.WriteLine("Substraction Result :" + subresult);
+        Console.WriteLine("Substraction Result =" + subResult);
         break;
     case 3:
 
         double multiplicantionResult = number1 * number2;
 
-        Console.WriteLine("Multiplicantion Result :" + multiplicantionResult);
+        Console.WriteLine("Multiplicantion Result =" + multiplicantionResult);
         break;
     case 4:
         if (number2 == 0)
@@ -80,11 +91,190 @@ switch (operationNumber)
         else
         {
 
-            double divresult = number1 / number2;
+            double divResult = number1 / number2;
 
-            Console.WriteLine("Division Result :" + divresult);
+            Console.WriteLine("Division Result =" + divResult);
+        }
+        break;
+
+    case 5:
+
+        if (number2 == 0)
+        {
+            Console.WriteLine("Exponentiation Result = 1");
+        }
+        else if (number2 == 1)
+        {
+            Console.WriteLine("Exponentiation Result =" + number1);
+        }
+        else
+        {
+            double expoResult = 1;
+            for (int i = 0; i < number2; i++)
+            {
+                expoResult = expoResult * number1;
+            }
+            Console.WriteLine(" Exponentiation Result =" + expoResult);
         }
         break;
 
 
+    case 6:
+
+
+        if (number1 % 2 == 0)
+        {
+            Console.WriteLine(" Number1 is Even");
+        }
+        else
+        {
+            Console.WriteLine(" Number1 is Odd");
+        }
+
+        if (number2 % 2 == 0)
+        {
+            Console.WriteLine(" Number2 is Even");
+        }
+        else
+        {
+            Console.WriteLine(" Number2 is Odd");
+        }
+        break;
+
+    case 7:
+
+        bool isPrime = true;
+        if (number1 == 0 || number1 == 1)
+        {
+            isPrime = false;
+        }
+        else if (number1 == 2)
+        {
+            isPrime = true;
+        }
+        else if (number1 % 2 == 0)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 3; i < Math.Sqrt(number1); i = i + 2)
+            {
+                if (number1 % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine("Number1 is prime");
+        }
+        else
+        {
+            Console.WriteLine("Number1 is not prime");
+        }
+
+        if (number2 == 0 || number2 == 1)
+        {
+            isPrime = false;
+        }
+        else if (number2 == 2)
+        {
+            isPrime = true;
+        }
+        else if (number2 % 2 == 0)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 3; i < Math.Sqrt(number2); i = i + 2)
+            {
+                if (number2 % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine("Number2 number is prime");
+        }
+        else
+        {
+            Console.WriteLine("Number2 is not prime");
+        }
+        break;
+
+    case 8:
+        if (number1 == 0 || number1 == 1)
+        {
+            Console.WriteLine("Square root of number1 is " + number1);
+        }
+        else
+        {
+            Console.WriteLine("Square root of number1 is " + Math.Sqrt(number1));
+        }
+
+        if (number2 == 0 || number2 == 1)
+        {
+            Console.WriteLine("Square root of number2 is " + number2);
+        }
+        else
+        {
+            Console.WriteLine("Square root of number2 is " + Math.Sqrt(number2));
+        }
+
+        break;
+
+    case 9:
+        int mirrored1 = 0;
+        int mirrored2 = 0;
+        int numb1 = Convert.ToInt32(number1);
+        while (numb1 != 0)
+        {
+            mirrored1 = (mirrored1 * 10) + (numb1 % 10);
+            numb1 = numb1 / 10;
+        }
+        Console.WriteLine("Mirrored number1 is " + mirrored1);
+
+        int numb2 = Convert.ToInt32(number2);
+        while (numb2 != 0)
+        {
+            mirrored2 = (mirrored2 * 10) + (numb2 % 10);
+            numb2 = numb2 / 10;
+        }
+        Console.WriteLine("Mirrored number2 is " + mirrored2);
+
+        break;
+
+    case 10:
+
+        int mirrored = 0;
+        numb1 = Convert.ToInt32(number1);
+        while (numb1 != 0)
+        {
+            mirrored = (mirrored * 10) + (numb1 % 10);
+            numb1 = numb1 / 10;
+        }
+        Console.WriteLine("Mirrored number is " + mirrored);
+
+        if (mirrored == number1)
+        {
+            Console.WriteLine("The number is palindrom");
+        }
+        else
+        {
+            Console.WriteLine("The number is not palindrom ");
+        }
+        break;
+
+
+
 }
+
