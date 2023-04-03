3c4d82e796e4b57080e47607ca91836c8847e8c6
Diana <100789048+Mumunz@users.noreply.github.com>
2023-04-03 19:28:50 +0300

    More operations to calculator (#2)
    * [LEAR-2] added basic operations
    
    * Update
    
    * Update
    
    * More operations
    
    * added all operations
    
    * Update
    
    * Update with methods
    
    * Update new classes
    
    * Separated code classes and projects
    
    ---------
    
    Co-authored-by: Avram Rotar <avram.rotar@gmail.com>

---
+++ CalculatorConsole/CalculatorService.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsole
+{
+    internal static class CalculatorService
+    {
+        public static void ShowOperationsMenu()
+        {
+            Console.WriteLine("\n Choose an operation : ");
+
+            Console.WriteLine("\n1.Sum");
+            Console.WriteLine("2.Subtraction");
+            Console.WriteLine("3.Multiplication");
+            Console.WriteLine("4.Divison");
+            Console.WriteLine("5.Exponentiation");
+            Console.WriteLine("6.Even / Odd");
+            Console.WriteLine("7.Prime");
+            Console.WriteLine("8.Square root");
+            Console.WriteLine("9.Mirrored");
+            Console.WriteLine("10.Palindrome");
+            Console.WriteLine("11.Superpalidrome ");
+            Console.WriteLine("12.Modulus of x");
+            Console.WriteLine("13.Display palindrome and superpalindrome numbers between 1 and 10000");
+        }
+
+        public static void DoOperation()
+
+        {
+            string operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "1":
+                    {
+                        CalculatorManager.CalculateTwoDoubleNumberSum();
+                        break;
+                    }
+
+                case "2":
+                    {
+                        CalculatorManager.CalculateTwoDoubleNumberSubstraction();
+                        break;
+                    }
+                case "3":
+                    {
+                        CalculatorManager.CalculateTwoDoubleNumberMultiplication();
+                        break;
+                    }
+                case "4":
+                    {
+                        CalculatorManager.CalculateTwoDoubleNumberDivision();
+                        break;
+                    }
+                case "5":
+                    {
+                        CalculatorManager.CalculateDoubleNumberExponentiation();
+                        break;
+                    }
+
+                case "6":
+                    {
+                        CalculatorManager.CalculateNumberParity();
+                        break;
+                    }
+                case "7":
+                    {
+                        CalculatorManager.ShowIfNumberIsPrime();
+
+                        break;
+                    }
+                case "8":
+                    {
+                        CalculatorManager.CalculateSquareRootOfDoubleNumber();
+
+                        break;
+                    }
+                case "9":
+                    {
+                        CalculatorManager.CalculateMirroredOfDoubleNumber();
+                        break;
+                    }
+                case "10":
+                    {
+                        CalculatorManager.ShowIfNumberIsPalindrome();
+                        break;
+                    }
+                case "11":
+                    {
+                        CalculatorManager.ShowIfNumberIsSuperPalindrome();
+
+                        break;
+                    }
+                case "12":
+                    {
+                        CalculatorManager.CalculateAbsoluteValueOfDoubleNumber();
+
+                        break;
+                    }
+
+                case "13":
+                    {
+                        CalculatorManager.ShowPalindromeAndSuperpalindromeBetween1And10000();
+                        break;
+                    }
+            }
+        }
+
+    }
+}
--- CalculatorConsole/Program.cs
+++ CalculatorConsole/Program.cs
@@ -1,90 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 using System;
 using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
 
-double number1 = 0;
-
-Console.WriteLine("Enter Number1 :");
-
-
-string theNumberAsAString = Console.ReadLine();
-
-
-while (!double.TryParse(theNumberAsAString, out number1))
+namespace CalculatorConsole
 {
-    Console.WriteLine(" \n This is not a number. Enter a number : ");
-    theNumberAsAString = Console.ReadLine();
-}
-
-Console.WriteLine(" \n The number is : " + number1);
-
-
-
-double number2 = 0;
-
-Console.WriteLine(" \n Enter Number2 :");
-
-string theNumberAsAString2 = Console.ReadLine();
-
-while (!double.TryParse(theNumberAsAString2, out number2))
-{
-    Console.WriteLine(" \n This is not a number. Entere a number : ");
-    theNumberAsAString2 = Console.ReadLine();
-}
-
-Console.WriteLine(" \nThe number is " + number2);
-
-Console.WriteLine(" \n Choose an operation : ");
-
-Console.WriteLine("\n1.Sum");
-Console.WriteLine("2.Subtraction");
-Console.WriteLine("3.Multiplication");
-Console.WriteLine("4.Divison");
-
-
-
-string opertaion = Console.ReadLine();
-
-double operationNumber = 0;
-
-while (!double.TryParse(opertaion, out operationNumber) || (operationNumber != 1 && operationNumber != 2 && operationNumber != 3 && operationNumber != 4))
-{
-    Console.WriteLine(" \n Choose a valid operation ");
-    opertaion = Console.ReadLine();
-}
-
-
-switch (operationNumber)
-{
-    case 1:
-        double sumresult = number1 + number2;
-
-        Console.WriteLine("Sum Result :" + sumresult);
-        break;
-    case 2:
-        double subresult = number1 - number2;
-
-        Console.WriteLine("Substraction Result :" + subresult);
-        break;
-    case 3:
-
-        double multiplicantionResult = number1 * number2;
-
-        Console.WriteLine("Multiplicantion Result :" + multiplicantionResult);
-        break;
-    case 4:
-        if (number2 == 0)
+    internal class Program
+    {
+        static void Main(string[] args)
         {
-            Console.WriteLine("Number2 must not be 0");
+            do
+            {
+                Console.Clear();
+                CalculatorService.ShowOperationsMenu();
+                CalculatorService.DoOperation();
+                Console.WriteLine("Would you like to do another operation? y/n");
+            } while (Console.ReadLine().ToLower() == "y");
         }
-        else
-        {
-
-            double divresult = number1 / number2;
-
-            Console.WriteLine("Division Result :" + divresult);
-        }
-        break;
-
-
+    }
 }
+
+       
