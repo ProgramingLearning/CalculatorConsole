8491aac6887df352f399e286198251513357554a
Avram Rotar <avram.rotar@gmail.com>
2023-06-29 13:43:33 +0300

    add a status label binding with event notifications

---
+++ CalculatorLogic/Operation.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculatorLogic
+{
+    public enum Operation
+    {
+        None = 0,
+        Sum = 1,
+        Sub = 2,
+        Mult = 3,
+        Div = 4,
+    }
+}
--- CalculatorUiWPF/MainWindow.xaml.cs
+++ CalculatorUiWPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using CalculatorLogic;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace CalculatorUiWPF
@@ -6,19 +8,79 @@ namespace CalculatorUiWPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private string operation;
-        private string number1;
+        private Operation _operation = Operation.None;
+
+        private Operation operation
+        {
+            get { return _operation; }
+            set
+            {
+                _operation = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(LabelText)));
+            }
+        }
+
+        private string _number1;
+
+        private string number1
+        {
+            get { return _number1; }
+            set
+            {
+                _number1 = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(LabelText)));
+            }
+        }
+
+        private string OutputTextBlockP
+        {
+            get
+            {
+                if (this.OutputTextBlock != null)
+                {
+                    return this.OutputTextBlock.Text;
+                }
+
+                return string.Empty;
+            }
+            set
+            {
+                this.OutputTextBlock.Text = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(LabelText)));
+            }
+        }
+
+
         private bool hasOperationBeenPressed;
         private bool hasResultBeenPressed;
         private bool canShowResult;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public string LabelText
         {
             get
             {
-                return $"{number1} {operation} {this.OutputTextBlock.Text}";
+                return $"{number1} {ConvertEnum()} {this.OutputTextBlockP}";
+            }
+        }
+
+        private string ConvertEnum()
+        {
+            switch (operation)
+            {
+                case Operation.Sum:
+                    return "+";
+                case Operation.Sub:
+                    return "-";
+                case Operation.Mult:
+                    return "*";
+                case Operation.Div:
+                    return "/";
+                default:
+                    return string.Empty;
             }
         }
 
@@ -29,9 +91,17 @@ namespace CalculatorUiWPF
 
         public void ShowNumber_Click(object sender, RoutedEventArgs e)
         {
+            SetFlagsAfterShowNumberClick();
+
+            string content = (string)((Button)sender).Content;
+            SetContentToOutputText(content);
+        }
+
+        private void SetFlagsAfterShowNumberClick()
+        {
             if (hasOperationBeenPressed || hasResultBeenPressed)
             {
-                OutputTextBlock.Text = string.Empty;
+                this.OutputTextBlockP = "0";
 
                 if (hasOperationBeenPressed)
                 {
@@ -39,100 +109,130 @@ namespace CalculatorUiWPF
                 }
             }
 
-            string content = (string)((Button)sender).Content;
-            this.OutputTextBlock.Text += content;
-
             hasOperationBeenPressed = false;
             hasResultBeenPressed = false;
         }
 
+        private void SetContentToOutputText(string content)
+        {
+            if (this.OutputTextBlockP == "0")
+            {
+                if (content == ".")
+                {
+                    this.OutputTextBlockP += content;
+                }
+                else
+                {
+                    this.OutputTextBlockP = content;
+                }
+            }
+            else
+            {
+                this.OutputTextBlockP += content;
+            }
+
+        }
+
         public void DeleteNumber_Click(object sender, RoutedEventArgs e)
         {
-            OutputTextBlock.Text = string.Empty;
+            this.OutputTextBlockP = "0";
             canShowResult = false;
         }
 
         public void Division_Click(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(OutputTextBlock.Text))
+            if (!string.IsNullOrEmpty(this.OutputTextBlockP))
             {
-                operation = "division";
-                number1 = OutputTextBlock.Text;
+                TryCalculate();
+                operation = Operation.Div;
+                number1 = this.OutputTextBlockP;
                 hasOperationBeenPressed = true;
             }
         }
 
         public void Substraction_Click(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(OutputTextBlock.Text))
+            if (!string.IsNullOrEmpty(this.OutputTextBlockP))
             {
-                operation = "substraction";
-                number1 = OutputTextBlock.Text;
+                TryCalculate();
+                operation = Operation.Sub;
+                number1 = this.OutputTextBlockP;
                 hasOperationBeenPressed = true;
             }
         }
 
         public void Multiplication_Click(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(OutputTextBlock.Text))
+            if (!string.IsNullOrEmpty(this.OutputTextBlockP))
             {
-                operation = "multiplication";
-                number1 = OutputTextBlock.Text;
+                TryCalculate();
+                operation = Operation.Mult;
+                number1 = this.OutputTextBlockP;
                 hasOperationBeenPressed = true;
             }
         }
 
         public void Sum_Click(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(OutputTextBlock.Text))
+            if (!string.IsNullOrEmpty(this.OutputTextBlockP))
             {
-                operation = "sum";
-                number1 = OutputTextBlock.Text;
+                TryCalculate();
+                operation = Operation.Sum;
+                number1 = this.OutputTextBlockP;
                 hasOperationBeenPressed = true;
             }
         }
 
         public void Result_Click(object sender, RoutedEventArgs e)
         {
+            TryCalculate();
+        }
+
+        private void TryCalculate()
+        {
             if (canShowResult)
             {
                 if (!hasResultBeenPressed)
                 {
                     var numberD1 = double.Parse(number1);
-                    var numberD2 = double.Parse(OutputTextBlock.Text);
-                    switch (operation)
-                    {
-                        case "division":
-                            {
-                                if (numberD2 != 0)
-                                {
-                                    OutputTextBlock.Text = CalculatorLogic.MathematicalOperations.GetDivResult(numberD1, numberD2).ToString();
-                                }
-                                break;
-                            }
-                        case "sum":
-                            {
-                                OutputTextBlock.Text = CalculatorLogic.MathematicalOperations.GetSumResult(numberD1, numberD2).ToString();
-                                break;
-                            }
-                        case "substraction":
-                            {
-                                OutputTextBlock.Text = CalculatorLogic.MathematicalOperations.GetSubResult(numberD1, numberD2).ToString();
-                                break;
-                            }
-
-                        case "multiplication":
-                            {
-                                OutputTextBlock.Text = CalculatorLogic.MathematicalOperations.GetMultiplicantionResult(numberD1, numberD2).ToString();
-                                break;
-                            }
-                        default:
-                            break;
-                    }
+                    var numberD2 = double.Parse(this.OutputTextBlockP);
+                    DoCalculation(numberD1, numberD2);
                 }
 
                 hasResultBeenPressed = true;
                 canShowResult = false;
+            }
+        }
+
+        private void DoCalculation(double numberD1, double numberD2)
+        {
+            switch (operation)
+            {
+                case Operation.Sum:
+                    {
+                        this.OutputTextBlockP = CalculatorLogic.MathematicalOperations.GetSumResult(numberD1, numberD2).ToString();
+                        break;
+                    }
+                case Operation.Sub:
+                    {
+                        this.OutputTextBlockP = CalculatorLogic.MathematicalOperations.GetSubResult(numberD1, numberD2).ToString();
+                        break;
+                    }
+                case Operation.Mult:
+                    {
+                        this.OutputTextBlockP = CalculatorLogic.MathematicalOperations.GetMultiplicantionResult(numberD1, numberD2).ToString();
+                        break;
+                    }
+                case Operation.Div:
+                    {
+                        if (numberD2 != 0)
+                        {
+                            this.OutputTextBlockP = CalculatorLogic.MathematicalOperations.GetDivResult(numberD1, numberD2).ToString();
+                        }
+                        break;
+                    }
+                default:
+                    break;
             }
         }
     }
